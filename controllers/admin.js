818f89d140df78962fd097fb7cc5f2152862ca8b
Uriel Medina <urimeba511@gmail.com>
2020-09-14 20:01:35 -0500

    *feat* JS Client side

--- controllers/admin.js
+++ controllers/admin.js
@@ -192,11 +192,12 @@ exports.getProducts = (req, res, next) => {
         });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+
     Product.findById(prodId)
         .then(product => {
-            if(!product){
+            if (!product) {
                 next(new Error('Product not found'))
             }
             fileHelper.deleteFile(product.imageUrl);
@@ -204,12 +205,14 @@ exports.postDeleteProduct = (req, res, next) => {
         })
         .then(() => {
             console.log('Product destroy');
-            res.redirect('/admin/products');
+            // res.redirect('/admin/products');
+            res.status(200).json({ message: 'Success' });
         })
         .catch(err => {
-            const error = new Error(err);
-            error.httpStatus = 500;
-            return next(error);
+            //     const error = new Error(err);
+            //     error.httpStatus = 500;
+            //     return next(error);
+            res.status(500).json({ message: 'Deleting product failed' });
         });
 
 }
